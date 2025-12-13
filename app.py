aa4687c3c9aa6bcd5eca86b9f1a3cfeb2bd10eab
andre <driekie248@gmail.com>
2025-12-13 18:51:15 +0100

    now renders index.html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder='public')
 
 @app.route('/')
 def home():
-    return render_template('vrm.html')  
+    return render_template('index.html')  
 
 if __name__ == '__main__':
     app.run(debug=True)
