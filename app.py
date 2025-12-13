0c9bf5d48528fe3eb1c54096d414906cf208a13f
andre <driekie248@gmail.com>
2025-12-13 18:52:40 +0100

    added route for overlay

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__, static_folder='public')
 def home():
     return render_template('index.html')  
 
+@app.route('/overlay')
+def overlay():
+    return render_template('overlay.html') 
+
 if __name__ == '__main__':
     app.run(debug=True)
